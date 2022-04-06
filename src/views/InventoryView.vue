a1d2dff8023350d5fc998bcbe19c58da24db1ae2
Justin Farley <justin.farley94@gmail.com>
2022-04-06 15:40:55 -0400

    Added in Inventory Table
    Basic outline of inventory table. With no functionality at all. Will enable functionality later.

--- src/views/InventoryView.vue
+++ src/views/InventoryView.vue
@@ -1,15 +1,21 @@
 <template>
-    <div>
-        <h1>Hello World</h1>
+    <div class="itemList">
+        <Item />
     </div>
 </template>
 
 <script>
 
+import Item from '../components/inventory/Item.vue'
+
 export default ({
     name: 'InventoryView',
     components: {
-        
+        Item
     }
 })
 </script>
+
+<style scoped>
+
+</style>
