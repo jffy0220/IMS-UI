90a77ede50d71302d3a156cb6551ad3efb2f93b4
Justin Farley <justin.farley94@gmail.com>
2022-04-07 15:56:38 -0400

    Added in Advanced Table and Inventory Example
    Advanced Tables and Inventory examples added. Next up is to create the navigation for the side and top.

--- src/inventory/Inventory.js
+++ src/inventory/Inventory.js
@@ -1,5 +1,11 @@
 import { Col, Row } from 'react-bootstrap'
 import InventoryGraph from './util/InventoryGraph'
+import AdvanceTable from 'common/AdvanceTable/AdvanceTable'
+import AdvanceTableFooter from 'common/AdvanceTable/AdvanceTableFooter'
+import AdvanceTablePagination from 'common/AdvanceTable/AdvanceTablePagination'
+import AdvanceTableSearchBox from 'common/AdvanceTable/AdvanceTableSearchBox'
+import AdvanceTableWrapper from 'common/AdvanceTable/AdvanceTableWrapper'
+import FalconComponentCard from 'common/FalconComponentCard'
 
 const audienceChart = {
     users: [
@@ -20,12 +26,144 @@ const audienceChart = {
     ]
 }
 
+const columns = [
+    {
+      accessor: 'name',
+      Header: 'Name'
+    },
+    {
+      accessor: 'email',
+      Header: 'Email'
+    },
+    {
+      accessor: 'age',
+      Header: 'Age'
+    }
+];
+
+const data = [
+    {
+      name: 'Anna',
+      email: 'anna@example.com',
+      age: 18
+    },
+    {
+      name: 'Homer',
+      email: 'homer@example.com',
+      age: 35
+    },
+    {
+      name: 'Oscar',
+      email: 'oscar@example.com',
+      age: 52
+    },
+    {
+      name: 'Emily',
+      email: 'emily@example.com',
+      age: 30
+    },
+    {
+      name: 'Jara',
+      email: 'jara@example.com',
+      age: 25
+    },
+    {
+      name: 'Clark',
+      email: 'clark@example.com',
+      age: 39
+    },
+    {
+      name: 'Jennifer',
+      email: 'jennifer@example.com',
+      age: 52
+    },
+    {
+      name: 'Tony',
+      email: 'tony@example.com',
+      age: 30
+    },
+    {
+      name: 'Tom',
+      email: 'tom@example.com',
+      age: 25
+    },
+    {
+      name: 'Michael',
+      email: 'michael@example.com',
+      age: 39
+    },
+    {
+      name: 'Antony',
+      email: 'antony@example.com',
+      age: 39
+    },
+    {
+      name: 'Raymond',
+      email: 'raymond@example.com',
+      age: 52
+    },
+    {
+      name: 'Marie',
+      email: 'marie@example.com',
+      age: 30
+    },
+    {
+      name: 'Cohen',
+      email: 'cohen@example.com',
+      age: 25
+    },
+    {
+      name: 'Rowen',
+      email: 'rowen@example.com',
+      age: 39
+    }
+];
+
 const Inventory = () => {
     return (
         <>
             <Row className="g-3 mb-3">
                 <Col xxl={8}>
                     <InventoryGraph chartData={audienceChart} className="mb-3" />
+                </Col>
+            </Row>
+            <Row className="g-3 mb-3">
+                <Col xxl={8}>
+                    <AdvanceTableWrapper
+                        columns={columns}
+                        data={data}
+                        sortable
+                        pagination
+                        perPage={5}
+                    >
+                        <Row className="flex-end-center mb-3">
+                            <Col xs="auto" sm={6} lg={4}>
+                                <AdvanceTableSearchBox table/>
+                            </Col>
+                        </Row>
+
+                        <AdvanceTable
+                            table
+                            headerClassName="bg-200 text-900 text-nowrap align-middle"
+                            rowClassName="align-middle white-space-nowrap"
+                            tableProps={{
+                                bordered: true,
+                                striped: true,
+                                className: 'fs--1 mb-0 overflow-hidden'
+                            }}
+                        />
+
+                        <div className="mt-3">
+                            <AdvanceTableFooter
+                                rowCount={data.length}
+                                table
+                                rowInfo
+                                navButtons
+                                rowsPerPageSelection
+                            />
+                        </div>
+
+                    </AdvanceTableWrapper>
                 </Col>
             </Row>
         </>
